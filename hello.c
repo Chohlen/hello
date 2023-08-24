e0b99803eac9cd54a68cf1ce4ddbd29eff3f9df5
chohlen <chohlen4@huskers.unl.edu>
2023-08-24 15:51:54 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Connor Hohlen
+ * Anok Timmothy
+ *
+ * 2023/08/24
+ *
+ * Printing out magor and names
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    //This prints the major and name
+    printf("Computer Engineering\n");
+    printf("Connor Hohlen!\n");
+
+    return 0;
+
+}
+
